0df81b1af3187eeea984702951cdcf151e1e7e9c
MSI\sisi2 <zeal24360207@fsgcl.jp>
2025-05-27 10:45:07 +0900

    確認問題　5.1

--- src/RectIf.java
+++ src/RectIf.java
@@ -6,35 +6,35 @@ public class RectIf extends MyFrame{
 		int i; 
 		for (i=0; i<9; i++) {
 		if (i==0) {
-			  fillRect(x,60,10,100);
+			  fillRect(x,40,10,40);
 			  
 		
 		}else if(i==1) {
-			  fillRect(x,70,10,100);
+			  fillRect(x,40,10,50);
 			  
 		}else if(i==2) {
-			  fillRect(x,80,10,100);
+			  fillRect(x,40,10,60);
 			  
 		}else if(i==3) {
-			  fillRect(x,90,10,100);
+			  fillRect(x,40,10,70);
 			  
 		}else if(i==4) {
-			  fillRect(x,100,10,100);
+			  fillRect(x,40,10,80);
 			  
 		}else if(i==5) {
-			  fillRect(x,110,10,100);
+			  fillRect(x,40,10,90);
 			  
 		}else if(i==6) {
-			  fillRect(x,120,10,100);
+			  fillRect(x,40,10,100);
 			  
 		}else if(i==7) {
-			  fillRect(x,130,10,100);
+			  fillRect(x,40,10,110);
 			  
 		}else if(i==8) {
-			  fillRect(x,140,10,100);
+			  fillRect(x,40,10,120);
 			  
 		}else {
-			  fillRect(x,150,10,100);
+			  fillRect(x,40,10,130);
 			  
 		
 			  
