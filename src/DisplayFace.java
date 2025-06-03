cea6b7c26ead7f19e762457c0a7fbbf477fa364b
MSI\sisi2 <zeal24360207@fsgcl.jp>
2025-06-03 10:17:17 +0900

    Displayface.java

--- src/DisplayFace.java
+++ src/DisplayFace.java
@@ -2,9 +2,9 @@
 public class DisplayFace extends MyFrame {
 	public void run() 
 	{
-		fillOval(39,40,50,100);
 		fillOval(130,40,50,100);
-		fillOval(30,200,150,10);
+		fillOval(230,40,50,100);
+		fillRect(130,200,150,10);
 	}
 
 }
