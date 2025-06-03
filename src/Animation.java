56aecbc4b646b4904295b3517de03a9670e660c4
MSI\sisi2 <zeal24360207@fsgcl.jp>
2025-06-03 11:40:23 +0900

    DisplayFaceAndTree

--- src/Animation.java
+++ src/Animation.java
@@ -1,30 +1,32 @@
 
 public class Animation extends MyFrame {
 	public void run() {
-		int y=200;
-		int x=30;
 		
+		int y=100;
+		int x=50;
 		
-		while(y>=80) {
-			clear();
-			
-			setColor(0,128,0);
-			fillOval(x,y,100,100);
-			y-=5;
-			sleep(0.1);
-			}
-			
 		
 		while(x<=170) {
 			clear();
 			
 			setColor(0,128,0);
 			fillOval(x,y,100,100);
+			x+=10;
+			sleep(0.1);
+			}
 			
-			x+=5;
+		
+		while(x>=50) {
+			clear();
+			
+			setColor(0,128,0);
+			fillOval(x,y,100,100);
+			
+			x-=10;
 			
 			sleep(0.1);
 		}
+		
 	}
 
 }
