3453921a073ee867297ea072e95a83d19f91b19e
MSI\sisi2 <zeal24360207@fsgcl.jp>
2025-06-09 09:34:44 +0900

    6-1 練習問題

---
+++ src/Car.java
@@ -0,0 +1,15 @@
+
+public class Car extends MyFrame {
+	public void run() {
+	        // Car body
+	        fillRect(50, 100, 200, 80);
+	        // Left wheel
+	        fillOval(70, 160, 50, 50);
+	        // Right wheel
+	        fillOval(180, 160, 50, 50);
+	        // Car roof
+	        fillRect(100, 60, 100, 40);
+	    
+	}
+
+}
